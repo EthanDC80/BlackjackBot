d0be9bc883c89408d380d75b2dd3d4839fd82ee9
nvarwandkar <nvarwandkar@exeter.edu>
2023-11-17 11:44:37 -0500

    update

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,13 +1,60 @@
-
 public class Blackjack {
     public static void main(String[] args) {
-        BlackjackGame game = new BlackjackGame();
-        
-        int money = 10;
+        int numSimulations = 10000;
+        int playerWins = 0;
+        int dealerWins = 0;
+        int draws = 0;
+
+        for (int i = 0; i < numSimulations; i++) {
+            boolean playerWin = simulateGame();
+            if (playerWin) {
+                playerWins++;
+            } else {
+                dealerWins++;
+            }
+        }
+
+        System.out.println("Player Wins: " + playerWins);
+        System.out.println("Dealer Wins: " + dealerWins);
+        System.out.println("Draws: " + draws);
+        System.out.println("Total Win Percentage: " + Double.valueOf(playerWins)/Double.valueOf(dealerWins+playerWins)*100);
+    }
+
+    private static boolean simulateGame() {
+        Deck deck = new Deck();
+        Player player = new Player();
+
+        // Deal initial cards
+        player.addToHand(deck.drawCard());
+        player.addToHand(deck.drawCard());
+
+        // Player's turn
+        while (player.getHandValue() <17) {
+            player.addToHand(deck.drawCard());
+        }
+
+        // Dealer's turn
+        Player dealer = new Player();
+        dealer.addToHand(deck.drawCard());
        
-        	game.startGame();
-        	
-        
-        
+
+        while (dealer.getHandValue() < 17) {
+            dealer.addToHand(deck.drawCard());
+        }
+
+        // Determine the winner
+        int playerValue = player.getHandValue();
+        int dealerValue = dealer.getHandValue();
+
+        if (playerValue > 21 || (dealerValue <= 21 && dealerValue >= playerValue)) {
+            // Dealer wins or player busts
+            return false;
+        } else if (dealerValue > 21 || playerValue > dealerValue) {
+            // Player wins
+            return true;
+        } else {
+            // Draw
+            return false;
+        }
     }
 }
--- src/Card.java
+++ src/Card.java
@@ -1,174 +1,79 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.Random;
 
 class Card {
-    private final String suit;
-    private final String rank;
+    int value;
 
-    public Card(String suit, String rank) {
-        this.suit = suit;
-        this.rank = rank;
-    }
-
-    public int getValue() {
-        if (rank.equals("A")) {
-            return 11; // Ace can be 1 or 11, default to 11
-        } else if (rank.equals("K") || rank.equals("Q") || rank.equals("J")) {
-            return 10; // Face cards are worth 10
-        } else {
-            return Integer.parseInt(rank);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return rank + " of " + suit;
+    public Card(int value) {
+        this.value = value;
     }
 }
 
 class Deck {
-    private final List<Card> cards;
+    private ArrayList<Card> cards;
 
     public Deck() {
         cards = new ArrayList<>();
-        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                cards.add(new Card(suit, rank));
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < 4; j++) {
+                cards.add(new Card(i));
             }
         }
-
-        Collections.shuffle(cards);
     }
 
     public Card drawCard() {
-        if (cards.isEmpty()) {
-            throw new IllegalStateException("Deck is empty");
+        if (!cards.isEmpty()) {
+            shuffle();
         }
-        return cards.remove(0);
+
+        int randomIndex = new Random().nextInt(cards.size());
+        return cards.remove(randomIndex);
+    }
+
+    private void shuffle() {
+        // Simple shuffle logic for demonstration purposes
+        for (int i = cards.size() - 1; i > 0; i--) {
+            int j = new Random().nextInt(i + 1);
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(j));
+            cards.set(j, temp);
+        }
     }
 }
 
-class Hand {
-    private final List<Card> cards;
+class Player {
+    private ArrayList<Card> hand;
 
-    public Hand() {
-        cards = new ArrayList<>();
+    public Player() {
+        hand = new ArrayList<>();
     }
 
-    public void addCard(Card card) {
-        cards.add(card);
+    public void addToHand(Card card) {
+        hand.add(card);
     }
 
-    public int getScore() {
-        int score = cards.stream().mapToInt(Card::getValue).sum();
+    public int getHandValue() {
+        int value = 0;
+        int numAces = 0;
 
-        // Handle Ace as 1 if the score is over 21
-        for (Card card : cards) {
-            if (card.getValue() == 11 && score > 21) {
-                score -= 10;
+        for (Card card : hand) {
+            value += card.value;
+
+            if (card.value == 1) {
+                numAces++;
             }
         }
 
-        return score;
+        while (numAces > 0 && value + 10 <= 21) {
+            value += 10;
+            numAces--;
+        }
+
+        return value;
     }
 
-    public List<Card> getCards() {
-        return cards;
-    }
-
-    @Override
-    public String toString() {
-        return cards.toString();
+    public ArrayList<Card> getHand() {
+        return hand;
     }
 }
-
-class BlackjackGame {
-    private final Deck deck;
-    private final Hand playerHand;
-    private final Hand dealerHand;
-    int money = 100;
-    
-    public BlackjackGame() {
-        deck = new Deck();
-        playerHand = new Hand();
-        dealerHand = new Hand();
-        
-    }
-
-    public void startGame() {
-        playerHand.addCard(deck.drawCard());
-        dealerHand.addCard(deck.drawCard());
-        playerHand.addCard(deck.drawCard());
-        dealerHand.addCard(deck.drawCard());
-
-        // Player's turn
-        playerTurn();
-
-        // Dealer's turn
-        dealerTurn();
-
-        // Determine the winner
-        determineWinner();
-    }
-
-    private void playerTurn() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Your hand: " + playerHand + " (Score: " + playerHand.getScore() + ")");
-            System.out.println("Dealer's face-up card: " + dealerHand.getCards().get(0));
-
-            System.out.print("Do you want to hit (h) or stand (s)? ");
-            String choice = scanner.nextLine().toLowerCase();
-
-            if (choice.equals("h")) {
-                playerHand.addCard(deck.drawCard());
-                if (playerHand.getScore() > 21) {
-                    System.out.println("Bust! Your hand is over 21. You lose.");
-                    System.exit(0);
-                }
-            } else if (choice.equals("s")) {
-                break;
-            }
-        }
-    }
-
-    private void dealerTurn() {
-        while (dealerHand.getScore() < 17) {
-            dealerHand.addCard(deck.drawCard());
-        }
-
-        System.out.println("Dealer's hand: " + dealerHand + " (Score: " + dealerHand.getScore() + ")");
-    }
-
-    private int determineWinner() {
-        int playerScore = playerHand.getScore();
-        int dealerScore = dealerHand.getScore();
-
-        System.out.println("Your hand: " + playerHand + " (Score: " + playerScore + ")");
-        System.out.println("Dealer's hand: " + dealerHand + " (Score: " + dealerScore + ")");
-
-        if (playerScore > 21) {
-            money -= 10;
-            System.out.println("Bust! Your hand is over 21. You lose.   " + money);
-        } else if (dealerScore > 21) {
-        	money += 10;
-            System.out.println("Dealer busts! You win.       " + money);
-        } else if (playerScore > dealerScore) {
-            money += 10;
-        	System.out.println("You win!    " + money);
-        } else if (playerScore < dealerScore) {
-           money -= 10;
-        	System.out.println("You lose.");
-        } else {
-            System.out.println("It's a tie!");
-        }
-        return money;
-    }
-}
-
 
